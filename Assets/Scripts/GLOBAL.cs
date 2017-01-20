2ed087bb59bab6e81e9b2d0ba9d7f7ff1ac4ff43
Eli Davis <eli.davis1995@gmail.com>
2017-01-20 02:54:43 -0500

    Init

---
+++ Assets/Scripts/GLOBAL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Lydia {
+
+	/// <summary>
+	/// Any global variables here please
+	/// </summary>
+	public static class GLOBAL {
+
+	}
+
+}
